ff677cd544bbfee049b999a11d177f312f4f30a1
Dusi Sarath Chandra <dusisarath@in.ibm.com>
2016-06-08 15:00:28 +0530

    Updated addition of request module in package.json

--- app.js
+++ app.js
@@ -6,25 +6,31 @@
 
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
-var express = require('express');
-
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
-
+//var express = require('express');
+//
+//// cfenv provides access to your Cloud Foundry environment
+//// for more info, see: https://www.npmjs.com/package/cfenv
+//var cfenv = require('cfenv');
+//
 var request = require('request');
-
-// create a new express server
-var app = express();
-
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
-
-// get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
+//
+//// create a new express server
+//var app = express();
+//
+//// serve the files out of ./public as our main files
+//app.use(express.static(__dirname + '/public'));
+//
+//// get the app environment from Cloud Foundry
+//var appEnv = cfenv.getAppEnv();
 
 // start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
-});
+//app.listen(appEnv.port, '0.0.0.0', function() {
+//  // print a message when the server starts listening
+//  console.log("server starting on " + appEnv.url);
+//});
+
+request('http://www.google.com', function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	    console.log(body) // Show the HTML for the Google homepage. 
+	  }
+	})
